f2adc90c5a68c582d46438c344c5a77eff7fda72
Andrew Cummings <cydonian.monk@gmail.com>
2019-10-20 09:46:23 -0500

    Update for KSP v1.8.0, new Unity, and .NET 4.7

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("© 2014-2019 Davorin Učakar, Ryan Bray, Andrew Cummings")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
